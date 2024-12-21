2db188750d7d481d8c4efd53c5ac7516c2bde056
Robonics <ezj213@gmail.com>
2024-12-21 10:14:14 -0500

    Refactored sky changes into separate file and allowed them to be configured

--- northstar/src/main/java/com/lightning/northstar/config/NorthstarConfigs.java
+++ northstar/src/main/java/com/lightning/northstar/config/NorthstarConfigs.java
@@ -6,16 +6,21 @@ public class NorthstarConfigs {
 	public static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
 	public static final ForgeConfigSpec SPEC;
 
-	public static final ForgeConfigSpec.ConfigValue<Boolean> TEMPERATURE_STUFF_ENABLED;
 	public static final ForgeConfigSpec.ConfigValue<Integer> OXYGEN_SEALER_MAX_VOLUME;
+	public static final ForgeConfigSpec.ConfigValue<Boolean> RENDER_WEATHER;
+	public static final ForgeConfigSpec.ConfigValue<Boolean> RENDER_SKY;
 
 	static {
 		BUILDER.push("Northstar Config");
 
-		TEMPERATURE_STUFF_ENABLED = BUILDER.comment("Should northstar handle temperature? (This will be automatically disabled if ColdSweat is installed)")
-			.define("Temperature Enabled", true);
-		OXYGEN_SEALER_MAX_VOLUME = BUILDER.comment("The maximum volume in blocks that the oxygen sealer will try to seal")
-			.define("Sealer Volume", 1000);
+		OXYGEN_SEALER_MAX_VOLUME = BUILDER.comment("The maximum volume in blocks that the oxygen sealer will try to seal. This is multiplied by the RPM of the machine. So 20 * 256 = 2560 max volume")
+			.define("Sealer Volume", 20);
+
+		// TODO: Move to client (Will never happen)
+		RENDER_SKY = BUILDER.comment("Allow Northstar to render the sky")
+			.define("Custom Sky Rendering", true);
+		RENDER_WEATHER = BUILDER.comment("Allow Northstar to render custom weather")
+			.define("Custom Weather Rendering", true);
 
 		BUILDER.pop();
 		SPEC = BUILDER.build();
